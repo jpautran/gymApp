c3df17da46229fbfd237428ab7fe67499d769de8
Joao Paulo <jpautran1414@gmail.com>
2020-09-01 13:33:32 -0300

    adding db

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,20 +1,15 @@
-const fs = require('fs');
-const data = require("../data.json");
-const { age, date } = require('../utils');
-const intl = require('intl');
+const { age, date } = require('../../lib/utils');
+const db = require('../../config/db');
+const { restart } = require('nodemon');
 
-// INDEX
-exports.index =  function(req, res) {
-    return res.render("instructors/index", { instructors: data.instructors });
-}
-
-// CREATE
-exports.create = function(req, res) {
-    return res.render('instructors/create');
-}
-
-// POST
-exports.post  = function(req, res) {
+module.exports = {
+    index(req, res) {
+        return res.render("instructors/index");
+    },
+    create(req, res) {
+        return res.render('instructors/create');
+    },
+    post(req, res) {
         const keys = Object.keys(req.body);
     
         for(key of keys) {
@@ -23,107 +18,52 @@ exports.post  = function(req, res) {
             }
         }
 
-        let  {avatar_url, birth, name, services, gender} = req.body;
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id
+        `
 
-        birth = Date.parse(birth);
-        const created_at = Date.now();
-        const id = Number(data.instructors.length + 1);
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso
+        ]
 
-        data.instructors.push({
-            id,
-            avatar_url,
-            name,
-            birth,
-            gender,
-            services,
-            created_at
+        db.query(query, values, function(err, results) {
+            if(err) return res.send("Database Error!");
+
+            return res.redirect(`/instructors/${results.rows[0].id}`);
         });
 
-        fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-            if (err) return res.send("Write file error!");
-            return res.redirect(`/instructors/${id}`);
-        });
-}
-
-// SHOW
-exports.show = function(req, res) {
-    const { id } = req.params;
-
-    const foundInstructor = data.instructors.find(function(instructor) {
-        return id == instructor.id;
-    });
-
-    if(!foundInstructor) return res.send("Instructor not found!");
-
-    const instructor = {
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-        services: foundInstructor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
-
-    return res.render("instructors/show", { instructor });
-}
-
-// EDIT
-exports.edit = function(req, res) {
-    const { id } = req.params;
-
-    const foundInstructor = data.instructors.find(function(instructor) {
-        return id == instructor.id;
-    });
-
-    if(!foundInstructor) return res.send("Instructor not found!");
-
-    const instructor = {
-        ...foundInstructor,
-        birth: date(foundInstructor.birth).iso
-    }
-
-    return res.render('instructors/edit', { instructor });
-}
-
-// SAVE
-exports.put = function(req, res) {
-    const { id } = req.body;
-    let index = 0;
-
-    const foundInstructor = data.instructors.find(function(instructor, foundIndex) {
-        if(id == instructor.id) {
-            index = foundIndex;
-            return true;
+    },
+    show(req, res) {
+        return
+    },
+    edit(req, res) {
+        return
+    },
+    put(req, res) {
+        const keys = Object.keys(req.body);
+    
+        for(key of keys) {
+            if(req.body[key] == "") {
+                return res.send('Please, fill all the fields!');
+            }
         }
-    });
 
-    if(!foundInstructor) return res.send("Instructor not found!");
-
-    const instructor = {
-        ...foundInstructor,
-        ...req.body,
-        birth: Date.parse(req.body.birth),
-        id: Number(req.body.id)
-    }
-
-    data.instructors[index] = instructor;
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Write error!")
-        return res.redirect(`/instructors/${id}`);
-    });
-}
-
-// DELETE
-exports.delete = function(req, res) {
-    const { id } = req.body;
-
-    const filteredInstructors = data.instructors.filter(function(instructor) {
-        return instructor.id != id;
-    });
-
-    data.instructors = filteredInstructors;
-
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-        if(err) return res.send("Write file error!")
-        return res.redirect("/instructors");
-    });
+        return
+    },
+    delete(req, res) {
+        return
+    },
 }

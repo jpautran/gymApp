0d03067df9d3ee1de23a69780f29b4532ed9493f
Joao Paulo <jpautran1414@gmail.com>
2020-09-15 17:05:03 -0300

    pagination 2

--- public/scripts.js
+++ public/scripts.js
@@ -9,7 +9,7 @@ for (item of menuItems) {
 
 // PAGINAÇÃO
 
-function pagination(selectedPage, totalPages) {
+function paginate(selectedPage, totalPages) {
     let pages = [],
         oldPage
 
@@ -36,3 +36,20 @@ function pagination(selectedPage, totalPages) {
     
     return pages;
 }
+
+const pagination = document.querySelector(".pagination");
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total);
+
+let elements = "";
+
+for (let page of pages) {
+    if(String(page).includes("...")) {
+        elements += `<span>${page}</span>`
+    } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+    }
+}
+
+pagination.innerHTML = elements;
